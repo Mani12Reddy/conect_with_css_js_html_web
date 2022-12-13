cc1f0a91683065473a1b9ea25942d1d2f102fbe8
Mani12Reddy <reddymani2000@gmail.com>
2022-12-13 11:48:19 +0530

    first commit

---
+++ j1.js
@@ -0,0 +1,30 @@
+function click1(){
+    let h4Tag =document.querySelectorAll('h4')[0];
+    let pTag1=document.querySelectorAll('p')[0];
+    h4Tag.innerText = "NARUTO";
+    h4Tag.style.color = "gostwhite";
+    pTag1.innerText = "TIGER TEXT";
+    pTag1.style.color = "white";
+    pTag1.style.backgroundColor="red";
+    pTag1.style.textAlign = "center"
+}
+function click2(){
+    let h4Tag1 =document.querySelectorAll('h4')[1];
+    let pTag3=document.querySelectorAll('p')[1];
+    h4Tag1.innerText = "NARUTO";
+    h4Tag1.style.color = "gostwhite";
+    pTag3.innerText = "MANI TEXT";
+    pTag3.style.color = "white";
+    pTag3.style.backgroundColor="red";
+    pTag3.style.textAlign = "center"
+}
+function click3(){
+    let h4Tag2 =document.querySelectorAll('h4')[2];
+    let pTag2=document.querySelectorAll('p')[2];
+    h4Tag2.innerText = "NARUTO";
+    h4Tag2.style.color = "gostwhite";
+    pTag2.innerText = "MANI BIKE";
+    pTag2.style.color = "white";
+    pTag2.style.backgroundColor="red";
+    pTag2.style.textAlign = "center"
+}
